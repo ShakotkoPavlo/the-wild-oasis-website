9cd5df136d912b3e5274e157419c166b0164528a
Pavlo Shakotko <pavel.shakotko@budgetthuis.nl>
2026-04-24 15:28:47 +0200

    next suspense cabins and routing

---
+++ app/_components/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Secret Page</h1>
+    </div>
+  );
+}
---
+++ app/account/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Account",
+  description: "This is the account page of the application.",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Account Page</h1>
+      <p>This is the account page.</p>
+    </div>
+  );
+}
